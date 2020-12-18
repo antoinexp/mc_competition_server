fb76ba8a1b1b1f66bf761d9e35c7edb859c86fe7
antoinexp <antoinexp@gmail.com>
2020-12-18 09:52:31 +0100

    migrations (django)

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 DIRECTORY=`dirname $0`
 cd $DIRECTORY
 pip3 install -r requirements.txt --user
+#python3 manage.py makemigrations
+#python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
